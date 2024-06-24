3d8f3b9085c90cc9332abb8df62c0cfcfcfa8b4d
DESKTOP-27BA3UJ\Meverik <kovalenko1997gleb@gmail.com>
2024-06-24 21:54:34 +0300

    First test on Python Selenium

---
+++ Tests/FirstTestCase.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+
+service = Service()
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
+driver = webdriver.Chrome(service=service, options=options)
+
+driver.get("https://opensource-demo.orangehrmlive.com/")
+driver.implicitly_wait(1)
+
+driver.find_element(By.NAME, "username").send_keys("Admin")
+driver.find_element(By.NAME, "password").send_keys("admin123")
+driver.find_element(By.CLASS_NAME, "oxd-button").click()
+actual_title = driver.title
+exp_title = "OrangeHRM"
+
+if actual_title == exp_title:
+    print("test passed")
+else:
+    print("test failed")
+
+#driver.close()
